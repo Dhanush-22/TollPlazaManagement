e1527a297253f33e877f1d1fe6a6a52c23e0a02d
Dhanush-22 <bedredhanush37@gmail.com>
2022-04-23 17:41:42 +0530

    ""

--- myProject/urls.py
+++ myProject/urls.py
@@ -22,5 +22,4 @@ from django.conf.urls.static import static #added
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('root.urls')),
-    path('accounts/',include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
